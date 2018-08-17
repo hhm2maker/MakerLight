7e634daeba525300d3006ca364489681b62af5ef
DESKTOP-QV1R0KT\Administrator <1248694620@qq.com>
2018-08-17 21:38:46 +0800

    2018.8.17

---
+++ MakerLight/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MakerLight.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MakerLight
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// 关于页面
+        /// </summary>
+        AboutUserControl auc;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Width = SystemParameters.WorkArea.Width * 0.8;
+            Height = SystemParameters.WorkArea.Height * 0.8;
+
+            auc = new AboutUserControl();
+            gMain.Children.Add(auc);
+        }
+    }
+}
---
+++ MakerLight/View/AboutUserControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MakerLight.View
+{
+    /// <summary>
+    /// AboutUserControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class AboutUserControl : UserControl
+    {
+        public AboutUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var btn = (Button)sender;
+            btn.IsEnabled = false;
+            var ani = new ColorAnimation(Color.FromRgb(255, 0, 0), new Duration(TimeSpan.FromSeconds(2)));
+            ani.Completed += (s, a) => btn.IsEnabled = true;
+            btn.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+            btn.Background.BeginAnimation(SolidColorBrush.ColorProperty, ani);
+        }
+    }
+}
